696541327de45cd62c55e12663bba68a511639b5
Sherief Farid <sherief@farid.cloud>
2019-03-12 03:28:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,37 @@
-# Add  code here!
+# 
+# 
+# def prime?(number)
+#   if number%2 || number == 1
+#     return flase
+#   else 
+#       
+#   end
+#     
+# end
+
+def divisible_by_remaining_odd_numbers?(n)
+  potential_divisors = (3..Math.sqrt(n)).step(2).to_a
+  divisible = false
+  potential_divisors.each do |x|
+    if n % x != 0
+      potential_divisors.delete_if { |y| y % x == 0 }
+    else
+      divisible = true
+      potential_divisors = []
+    end
+  end
+
+  divisible
+
+end
+
+
+def prime?(n)
+  if n == 2
+    true
+  elsif n <= 1 || n.even?
+    false
+  else
+    divisible_by_remaining_odd_numbers?(n) ? false : true
+  end  
+end
